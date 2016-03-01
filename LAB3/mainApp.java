822d1593f8902d228400d283c788fc1b1de39305
HD Koh <hd.koh290293@gmail.com>
2016-03-01 17:16:37 +0800

    Update mainApp.java
    Added the option to save data set into .txt and to obtain data set from .txt files.

--- LAB3/mainApp.java
+++ LAB3/mainApp.java
@@ -1,42 +1,84 @@
 package cz2001_lab3;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class mainApp {
     // Selections of n, random methods, sorting methods
     private final static int n = 10;    
     // (1) random Generator
     // (2) Increasing order
     // (3) Decreasing order
-    private final static int r = 1;
+    // (4) Use data from file
+    private final static int r = 4;
     // (1) mergeSort
     // (2) quickSort
     private final static int s = 1;     
+    private static File file = new File("dataset.txt");
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Declaration of variables
         long startTime;
         long endTime;
+        int[] bArr = new int[n];
         
         // Instantiation of objects
         numGen gen = new numGen(n);
         mergeSort mSort = new mergeSort(n);
         quickSort qSort = new quickSort(n);
-
-        if (r == 1)
+        
+        if (r <= 3 && r >= 1) {
+            if (r == 1) {
             gen.randG1();   
-        else if (r==2)
+            bArr = gen.getArr();
+            }
+        else if (r == 2) {
             gen.randG2();
-        else if (r==3)
+            bArr = gen.getArr();
+            }
+        else if (r == 3) {
             gen.randG3();
+            bArr = gen.getArr();
+            }
+            PrintWriter pwStream = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+        for (int i = 0; i < n; i ++){
+            pwStream.print(bArr[i] + " ");
+            }
+        pwStream.close();
+        }
+        
+        else if (r == 4) {
+            try {
+                int j =0;
+                Scanner scStream = new Scanner(file);
+                while (scStream.hasNextInt()) {
+                    bArr[j] = scStream.nextInt();
+                    j++;
+                }
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("Error opening the input file!" + e.getMessage());
+                System.exit(0);
+            }
+            catch (IOException e) {
+                System.out.println("IO Error!" + e.getMessage());
+                System.exit(0);
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Check numbers within dataset and variable n. Mismatch!");
+                System.exit(0);
+            }
+        }
         
         // Print out array
-        gen.print();    
+        print(bArr);
         
         // mergeSort selection
         if (s == 1) {
             System.out.println("\n**** Start mergeSort ****");
             // Start timing of method
             startTime = System.nanoTime();
-            mSort.sort(gen.getArr(), 0, n-1);
+            mSort.sort(bArr, 0, n-1);
             endTime = System.nanoTime();
             // End timing of method
         }
@@ -46,12 +88,12 @@ public class mainApp {
             System.out.println("\n**** Start quickSort ****");
             // Start timing of method
             startTime = System.nanoTime();
-            qSort.sort(gen.getArr(), 0, n-1);
+            qSort.sort(bArr, 0, n-1);
             endTime = System.nanoTime();
             // End timing of method
         }
 
-        print(gen.getArr());      //Print sorted array
+        print(bArr);      //Print sorted array
         long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
         
         //Results
